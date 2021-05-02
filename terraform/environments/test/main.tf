6e64a31e1b595c8e56ccbdba3d2313431ea10f22
Thomas Weibel <thom@ikhono.net>
2021-05-02 13:50:22 +0200

    Set storage account name

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "tstate"
-    storage_account_name = "tstate423"
+    storage_account_name = "tstate5275"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
     # access_key           = "ACCESS_KEY"
