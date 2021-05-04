9b6b9795c94d839a5c5cc7e2fbfd7a8dcfd143bd
Thomas Weibel <thom@ikhono.net>
2021-05-04 11:46:26 +0200

    Update storage account name

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "tstate"
-    storage_account_name = "tstate5275"
+    storage_account_name = "tstate17968"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
   }
